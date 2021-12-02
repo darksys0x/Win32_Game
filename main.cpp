cc01d1c8e5fbead4534c63ee10aefb72dd803a26
hamad-SHE <eng_cis1@hotmail.com>
2021-12-02 18:38:37 +0300

    Handling Messages Box

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#pragma warning(push,0)
+#include <Windows.h>
+#pragma warning(pop)
+
+
+
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+// HINSTANCE hInstance : handle memory address input function to the main address
+//PSTR pointer to string long comand line
+int WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommnadLine, INT CmdShow)
+{
+    UNREFERENCED_PARAMETER(Instance);
+    UNREFERENCED_PARAMETER(PrevInstance);
+    UNREFERENCED_PARAMETER(CommnadLine);
+    UNREFERENCED_PARAMETER(CmdShow);
+
+
+    // this recive message 
+
+    WNDCLASSEXA WindowClass = { sizeof(WNDCLASSEXA) };
+    HWND WindowHandle = 0;
+
+
+    //WindowClass.cbSize = sizeof(WNDCLASSEXA);
+
+    WindowClass.style = 0;
+
+    WindowClass.lpfnWndProc = MainWndProc; // long pointer proc
+
+    WindowClass.cbClsExtra = 0;
+
+    WindowClass.cbWndExtra = 0;
+
+    WindowClass.hInstance = Instance;
+
+    WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
+
+    WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
+
+    WindowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+    WindowClass.lpszMenuName = NULL;
+
+    WindowClass.lpszClassName = "Hamad_Game_WIDOWCLASS";
+
+    WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassExA(&WindowClass)) // if fails it will be return 0;
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    WindowHandle = CreateWindowExA(WS_EX_CLIENTEDGE, WindowClass.lpszClassName,"The window Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 240, 120, NULL, NULL, Instance, NULL);
+
+    if (WindowHandle == NULL) //if fails it will be return 0;
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+
+    MSG Message = { 0 };
+
+    while (GetMessageA(&Message, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Message);
+        DispatchMessageA(&Message);
+    }
+    return 0;
+}
+
+
+LRESULT CALLBACK MainWndProc(
+    HWND hwnd,        // handle to window
+    UINT uMsg,        // message identifier
+    WPARAM wParam,    // first message parameter
+    LPARAM lParam)    // second message parameter
+{
+
+    switch (uMsg)
+    {
+    case WM_CREATE:
+        // Initialize the window. 
+        return 0;
+
+    case WM_PAINT:
+        // Paint the window's client area. 
+        return 0;
+
+    case WM_SIZE:
+        // Set the size and position of the window. 
+        return 0;
+
+    case WM_DESTROY:
+        // Clean up window-specific data objects. 
+        return 0;
+
+        // 
+        // Process other messages. 
+        // 
+
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
